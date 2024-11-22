10d794f9a1546070ce9701d19cd65f102801b209
Kirill <km220410@gmail.com>
2024-11-22 18:25:12 +0000

    обновил

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 def main():
-    url = "https://www.52shuku.vip/yanqing/hx4r_2828.html"
+    url = "https://tl.rulate.ru/book/16292/327323/ready_new"
     response = requests.get(url)
     print(response.status_code)
-    
-    with open('output.html', 'a', encoding='utf-8') as file:
-        file.write(response.text)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    part = soup.find("div", class_="text-container")
+    with open("page.txt", 'a', encoding="UTF-8") as file:
+        file.write(part.text)
 
 
 if __name__ == "__main__":
